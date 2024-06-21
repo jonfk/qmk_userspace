c3f27f27a76ec3f4c63f20401901ba405b512b8a
Jonathan Fok kan <jonathan@fokkan.ca>
2024-06-21 16:50:09 -0400

    Updated comment based on layout of lower

--- keyboards/planck/rev7/keymaps/jonfk/keymap.c
+++ keyboards/planck/rev7/keymaps/jonfk/keymap.c
@@ -94,20 +94,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
+ * |      |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   (  |   )  |   ?  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   ?  |   +  |   {  |   }  |  |   |
+ * | Del  |   `  |   *  |   +  |   =  |      |   |  |   /  |   {  |   }  |   \  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
+ * |      |   ~  |      |   "  |   '  |      |      |      |   [  |   ]  |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_QUES,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_QUES, KC_BSPC,
+    KC_DEL,  KC_GRV,  KC_ASTR, KC_PLUS, KC_EQL,  _______, KC_PIPE, KC_SLSH, KC_LCBR, KC_RCBR, KC_BSLS, _______,
+    _______, KC_TILD, _______, KC_DQUO, KC_QUOT, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______, 
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Raise
